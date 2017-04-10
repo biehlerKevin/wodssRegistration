5c78121668620a58694102a7f415c6cf86b8ffd4
Kevin Biehler <kevin_biehler@hotmail.com>
2017-04-10 17:07:00 +0200

    fixed startup error

--- src/app/shared/translation/translate.ts
+++ src/app/shared/translation/translate.ts
@@ -14,11 +14,14 @@ export const TRANSLATIONS = new OpaqueToken('translations');
 
 
 //all translations
-export const dictionary = {
-    [LANG_DE_NAME]: LANG_DE_TRANS,
-    [LANG_EN_NAME]: LANG_EN_TRANS,
-};
+//export const dictionary = {
+//    [LANG_DE_NAME]: LANG_DE_TRANS,
+//    [LANG_EN_NAME]: LANG_EN_TRANS,
+//};
 
+export const dictionary = {};
+dictionary[LANG_DE_NAME] = LANG_DE_TRANS;
+dictionary[LANG_EN_NAME] = LANG_EN_TRANS;
 
 //providers
 export const TRANSLATION_PROVIDERS = [
